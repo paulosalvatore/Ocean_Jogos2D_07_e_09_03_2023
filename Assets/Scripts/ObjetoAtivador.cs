7457800770711e2340f539f56e5f9ae052f3bdab
Paulo Salvatore <salvatoregames@gmail.com>
2023-03-09 10:29:07 -0300

    Instruções de implementação

--- Assets/Scripts/ObjetoAtivador.cs
+++ Assets/Scripts/ObjetoAtivador.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class ObjetoAtivador : MonoBehaviour
 {
+    /*
+    Esse é um comentário que não será lido pelo código
+
+    O que vamos implementar:
+    - Detectar que o objeto (o objeto ativador) está em contato com algum trigger
+    - Pegar um objeto específico (qualquer outro objeto, definido na cena) e desativá-lo
+    - Pegar outro objeto específico (qualquer outro objeto, definido na cena) e ativá-lo
+    */
+
     // Start is called before the first frame update
     void Start()
     {
